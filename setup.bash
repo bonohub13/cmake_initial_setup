d7180a28684b9d54e60fd4622ad55a8e89f0c760
Kensuke Saito <ken.saito.0813@gmail.com>
2020-04-21 05:26:54 +0900

    fixed a bug in setup.bash

--- setup.bash
+++ setup.bash
@@ -5,5 +5,5 @@ mkdir ~/.cmake_init && cp cmake_init.bash ~/.cmake_init
 if [ $(find ~ | grep ".cmake_init/cmake_init.bash") = ~/.cmake_init/cmake_init.bash ]
 then
 	echo "" >> $HOME/.bashrc
-	echo ". $HOME/.cmake_init/cmake_init.bash" >> "$HOME/.bashrc"
+	echo "alias cmake-init=\". $HOME/.cmake_init/cmake_init.bash\"" >> "$HOME/.bashrc"
 fi
