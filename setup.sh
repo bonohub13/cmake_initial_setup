f29d5832625e2bed02f49e609cca83af2a669a6b
Kensuke Saito <ken.saito.0813@gmail.com>
2021-08-03 21:04:28 +0900

    fixed setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/sh
 
 cp -r ./.templates ~/
-mkdir -p ~/.cmake_init && cp cmake_init.bash ~/.cmake_init
-if [ -f "$HOME/.cmake_init/cmake_init.bash" ]
+mkdir -p ~/.cmake_init && cp cmake_init.sh ~/.cmake_init
+if [ -f "$HOME/.cmake_init/cmake_init.sh" ]
 then
 	echo >> $HOME/.bashrc
-	echo "alias cmake-init=\". $HOME/.cmake_init/cmake_init.bash\"" >> "$HOME/.bashrc"
+	echo "alias cmake-init=\". $HOME/.cmake_init/cmake_init.sh\"" >> "$HOME/.bashrc"
 fi
-#!/usr/bin/sh
