cc73fa6cfad380b6c028bf69d73e867d838722e5
Kensuke Saito <ken.saito.0813@gmail.com>
2021-08-03 20:47:42 +0900

    Optimized setup script for better

--- setup.bash
+++ setup.bash
@@ -2,8 +2,8 @@
 
 cp -r ./.templates ~/
 mkdir -p ~/.cmake_init && cp cmake_init.bash ~/.cmake_init
-if [ $(find ~ | grep ".cmake_init/cmake_init.bash") = ~/.cmake_init/cmake_init.bash ]
+if [ -f "$HOME/.cmake_init/cmake_init.bash" ]
 then
-	echo "" >> $HOME/.bashrc
+	echo >> $HOME/.bashrc
 	echo "alias cmake-init=\". $HOME/.cmake_init/cmake_init.bash\"" >> "$HOME/.bashrc"
 fi
